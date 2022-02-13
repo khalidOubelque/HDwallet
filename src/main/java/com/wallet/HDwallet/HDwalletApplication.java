f4e78e91410676f7d220ae902f4ece4d62ceccee
khalidoubelque <khalidoubelque@gmail.com>
2022-02-13 02:04:37 +0100

    WIP HD wallet using Java

---
+++ src/main/java/com/wallet/HDwallet/HDwalletApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.HDwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HDwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HDwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallet/HDwallet/HDwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.HDwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HDwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
